65cce43c00bf4b5f81acd61e27e06645cf8eedf2
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2013-01-26 15:10:55 +0100

    node server catch me running

---
+++ app.js
@@ -0,0 +1,15 @@
+var io = require('socket.io').listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('set nickname', function (name) {
+    socket.set('nickname', name, function () {
+      socket.emit('ready');
+    });
+  });
+
+  socket.on('msg', function () {
+    socket.get('nickname', function (err, name) {
+      console.log('Chat message by ', name);
+    });
+  });
+});
